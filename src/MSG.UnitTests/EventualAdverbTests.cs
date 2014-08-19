9d6c8bc591a818a1ff8998048ddd93c376693a43
Tim Collins <tim@timcollins.ie>
2014-08-19 14:10:31 +0100

    Split some unit tests out into their own classes.

---
+++ src/MSG.UnitTests/EventualAdverbTests.cs
@@ -0,0 +1,53 @@
+﻿using MSG.DomainLogic;
+using NUnit.Framework;
+
+namespace MSG.UnitTests
+{
+    [TestFixture]
+    class EventualAdverbTests
+    {
+        [Test]
+        public void VerifyEventualAdverbInteractively()
+        {
+            MoqUtil.SetupRandMock(21, 6, 1, 9, 1, 1, 1, 1);
+            string output = DomainFactory.Generator.GetSentences(1)[0];
+
+            MoqUtil.UndoMockRandomNumber();
+
+            Assert.IsTrue(output.Contains(" interactively "));
+        }
+
+        [Test]
+        public void VerifyEventualAdverbCredibly()
+        {
+            MoqUtil.SetupRandMock(17, 5, 9, 2, 1, 1, 1);
+            string output = DomainFactory.Generator.GetSentences(1)[0];
+
+            MoqUtil.UndoMockRandomNumber();
+
+            Assert.IsTrue(output.Contains(" credibly "));
+        }
+
+        [Test]
+        public void VerifyEventualAdverbStrategically()
+        {
+            MoqUtil.SetupRandMock(17, 5, 9, 10, 1, 1, 1);
+            string output = DomainFactory.Generator.GetSentences(1)[0];
+
+            MoqUtil.UndoMockRandomNumber();
+
+            Assert.IsTrue(output.Contains(" strategically "));
+        }
+
+        [Test]
+        public void VerifyEventualAdverbConservatively()
+        {
+            MoqUtil.SetupRandMock(17, 5, 9, 17, 1, 1, 1);
+            string output = DomainFactory.Generator.GetSentences(1)[0];
+
+            MoqUtil.UndoMockRandomNumber();
+
+            Assert.IsTrue(output.Contains(" conservatively "));
+        }
+    }
+}
