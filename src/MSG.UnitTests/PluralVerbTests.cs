ffc9a0c38b261aeb5cd950fb17408d001ee59e24
Tim Collins <tim@timcollins.ie>
2014-08-29 11:41:09 +0100

    Added a couple of other plural tests and added a default list of numbers.

--- src/MSG.UnitTests/PluralVerbTests.cs
+++ src/MSG.UnitTests/PluralVerbTests.cs
@@ -7,17 +7,52 @@ namespace MSG.UnitTests
     [TestFixture]
     class PluralVerbTests
     {
+        private List<int> _defaults;
+
+        [SetUp]
+        public void SetUpDefaultNumbers()
+        {
+            _defaults = new List<int> { 17, 5, 1, 8, 1, 11, 1, 1, 1 };
+        }
+
+        [TearDown]
+        public void UndoRandomNumberSetting()
+        {
+            MoqUtil.UndoMockRandomNumber();
+        }
+
         [Test]
         public void VerifyIdentifies()
         {
-            List<int> defaults = new List<int> { 17, 5, 1, 8, 1, 11, 2, 1, 1, 1 };
-
-            MoqUtil.SetupRandMock(defaults.ToArray());
+            _defaults.Insert(6, 2);
+            MoqUtil.SetupRandMock(_defaults.ToArray());
 
             string output = DomainFactory.Generator.GetSentences(1)[0];
-            MoqUtil.UndoMockRandomNumber();
 
             Assert.IsTrue(output.Contains("identifies"));
+        }
+
+        [Test]
+        public void VerifyAvoids()
+        {
+            _defaults.Insert(6, 3);
+            MoqUtil.SetupRandMock(_defaults.ToArray());
+
+            string output = DomainFactory.Generator.GetSentences(1)[0];
+
+            Assert.IsTrue(output.Contains("avoids"));
+        }
+
+        [Test]
+        public void VerifyMitigates()
+        {
+            _defaults.Insert(6, 4);
+            MoqUtil.SetupRandMock(_defaults.ToArray());
+
+
+            string output = DomainFactory.Generator.GetSentences(1)[0];
+
+            Assert.IsTrue(output.Contains("mitigates"));
         }
     }
 }
