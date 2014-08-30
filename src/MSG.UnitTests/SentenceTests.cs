033a20e5ebf9ab3e75fd40db7a6e378ef05fb2e8
Tim <tim@timcollins.ie>
2014-08-30 18:52:31 +0100

    Fixed SentenceTests.cs.

--- src/MSG.UnitTests/SentenceTests.cs
+++ src/MSG.UnitTests/SentenceTests.cs
@@ -13,15 +13,15 @@ namespace MSG.UnitTests
             // If GetSentences is called twice then there should be 2 sentences in the output.
             // Specify some numbers to get specific output and verify spacing.
 
-            MoqUtil.SetupRandMock(  17, 5, 1, 1, 1, 1, 1, 1, 
-                                    18, 5, 1, 1, 1, 1, 1, 3, 4, 6, 2, 5, 1, 2, 1);
+            MoqUtil.SetupRandMock(  17, 5, 1, 1, 1, 1, 1, 
+                                    18, 5, 1, 1, 1, 1, 3, 4, 6, 2, 5, 1, 2, 1);
             List<string> output = DomainFactory.Generator.GetSentences(2);
 
             MoqUtil.UndoMockRandomNumber();
 
             Assert.AreEqual(2, output.Count);
             Assert.AreEqual("We need to interactively streamline the process going forward.", output[0]);
-            Assert.AreEqual("We need to interactively streamline the process across the board; this is why we've got to 200% address the overarching issues going forward.", output[1]);
+            Assert.AreEqual("We need to interactively streamline the process across the board; this is why pursuing this route will enable us to credibly streamline the process within the industry.", output[1]);
         }
     }
 }
