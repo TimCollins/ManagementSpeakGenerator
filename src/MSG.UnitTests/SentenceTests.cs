1714580e0b69e3266660c004f1995d80da47566e
Tim Collins <tim@timcollins.ie>
2014-09-08 16:26:20 +0100

    Added unfixed unit test for vowel problem.

--- src/MSG.UnitTests/SentenceTests.cs
+++ src/MSG.UnitTests/SentenceTests.cs
@@ -66,5 +66,31 @@ namespace MSG.UnitTests
 
             Assert.AreEqual("Cross fertilization, investor confidence and branding drive the Managing Co-Head of Customer Relations; this is why our long-term change promotes the human resources relative to our peers.", output);
         }
+
+        //[Test]
+        //public void FixVowelAtStart()
+        //{
+        //    MoqUtil.SetupRandMock(new[] {19, 66, 1, 52, 249, 103, 98, 10, 49, 2, 75, 8, 3, 48, 68, 2, 86, 187, 140, 4, 88, 2, 87, 12, 1, 86});
+        //    string output = DomainFactory.Generator.GetSentences(1)[0];
+        //    MoqUtil.UndoMockRandomNumber();
+
+        //    // GetIndefiniteArticle() is passed "unified convergence ".
+        //    // The problem is that we are making an assumption that everything that starts 
+        //    // with a vowel should be prefixed with "an" but that's not always the case, 
+        //    // even for the same letter e.g. "an understanding" v "a unified".
+
+        //    // https://www.google.ie/search?client=opera&q=rules+around+a+v+an+for+vowels&sourceid=opera&ie=utf-8&oe=utf-8&channel=suggest&gws_rd=ssl
+        //    // http://www.writersdigest.com/online-editor/a-before-consonants-and-an-before-vowels-is-not-the-rule
+        //    // The real rule is this: You use the article “a” before words that start with a consonant sound and “an” before words that start with a vowel sound. For example, He has a unique point of view on the subject and talked about it for an hour. The “u” in “unique” makes the “Y” sound—a consonant sound—therefore you use “a” as your article, while the “h” in “hour” sounds like it starts with “ow”—a vowel sound.
+
+        //    Assert.AreEqual("A unified convergence incentivises the customers; nevertheless our mindsets influence the key people.", output);
+        //}
+
+        //25,1,7,70,36,16,7,245,204,123,57,7,3,96,346,148,108,28,67,1,28,8,97,38,10,25,
+        //We will go the extra mile to focus on outstanding, high-definition, action plans across the board, while scaling and document synergise an intellect as a Tier 1 company.
+
+        //17,34,1,11,28,76,4,3,86,243,158,12,14,97,
+        //The resource improves a customer-centric, innovation-driven, system.
+
     }
 }
