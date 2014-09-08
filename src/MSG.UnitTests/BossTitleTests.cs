8e2f3ce8941a945d5daaecd78a86f975193ecdfc
Tim Collins <tim@timcollins.ie>
2014-09-08 08:58:53 +0100

    Fixed the failing tests in BossTitleTests.cs. All tests now pass.

--- src/MSG.UnitTests/BossTitleTests.cs
+++ src/MSG.UnitTests/BossTitleTests.cs
@@ -199,10 +199,7 @@ namespace MSG.UnitTests
 
             string output = DomainFactory.Generator.GetSentences(1)[0];
             Assert.AreEqual("The Managing Director of Marketing culturally exceeds expectations at the individual, team and organizational level.", output);
-        }
-
-        //_defaults = new List<int> {1, 7, 1, 17, 1, 5, 6, 3, 2, 14, 8, 9, 19, 33};
-
+        }        
 
         [Test]
         public void VerifyBossAlt()
@@ -212,47 +209,54 @@ namespace MSG.UnitTests
             _defaults.ReplaceAt(6, 17);
             _defaults.RemoveAt(8);
             _defaults.RemoveAt(9);
+            _defaults.ReplaceAt(9, 3);
+            _defaults.Add(8);
             MoqUtil.SetupRandMock(_defaults.ToArray());
 
             string output = DomainFactory.Generator.GetSentences(1)[0];
-            Assert.AreEqual("The Group Chief Technical Officer of Marketing culturally exceeds expectations at the individual, team and organizational level.", output);
+            Assert.AreEqual("The Group Chief Technical Officer quickly avoids uncertainties as part of the plan.", output);
         }
 
-        //[Test]
-        //public void VerifyChiefSpacing()
-        //{
-        //    MoqUtil.SetupRandMock(2, 3, 14, 5);
+        [Test]
+        public void VerifyChiefSpacing()
+        {
+            _defaults.ReplaceAt(4, 2);
+            _defaults.ReplaceAt(7, 5);
+            _defaults.ReplaceAt(10, 1);
+            MoqUtil.SetupRandMock(_defaults.ToArray());
 
-        //    string boss = DomainFactory.Generator.GetBoss();
-        //    MoqUtil.UndoMockRandomNumber();
+            string output = DomainFactory.Generator.GetSentences(1)[0];
+            Assert.AreEqual("The Chief Operations Officer credibly addresses unknown unknowns at the individual, team and organizational level.", output);
+        }
 
-        //    Assert.IsNotNull(boss);
-        //    Assert.AreEqual("Chief Operations Officer ", boss);
-        //}
+        //_defaults = new List<int> {1, 7, 1, 17, 1, 5, 6, 3, 2, 14, 8, 9, 19, 33};
 
-        //[Test]
-        //public void VerifyGroupChiefSpacing()
-        //{
-        //    MoqUtil.SetupRandMock(2, 1, 16, 5);
+        [Test]
+        public void VerifyGroupChiefSpacing()
+        {
+            _defaults.ReplaceAt(4, 2);
+            _defaults.ReplaceAt(5, 1);
+            _defaults.ReplaceAt(6, 16);
+            _defaults.ReplaceAt(7, 5);
+            _defaults.ReplaceAt(10, 1);
+            MoqUtil.SetupRandMock(_defaults.ToArray());
 
-        //    string boss = DomainFactory.Generator.GetBoss();
-        //    MoqUtil.UndoMockRandomNumber();
+            string output = DomainFactory.Generator.GetSentences(1)[0];
+            Assert.AreEqual("The Group Chief Digital Officer 200% achieves efficiencies going forward.", output);
+        }
 
-        //    Assert.IsNotNull(boss);
-        //    Assert.AreEqual("Group Chief Digital Officer ", boss);
-        //}
+        [Test]
+        public void VerifyGlobalChiefSpacing()
+        {
+            _defaults.ReplaceAt(4, 2);
+            _defaults.ReplaceAt(5, 2);
+            _defaults.ReplaceAt(6, 16);
+            _defaults.ReplaceAt(7, 5);
+            _defaults.ReplaceAt(10, 1);
+            MoqUtil.SetupRandMock(_defaults.ToArray());
 
-        //[Test]
-        //public void VerifyGlobalChiefSpacing()
-        //{
-        //    MoqUtil.SetupRandMock(2, 2, 16, 5);
-
-        //    string boss = DomainFactory.Generator.GetBoss();
-        //    MoqUtil.UndoMockRandomNumber();
-
-        //    Assert.IsNotNull(boss);
-        //    Assert.AreEqual("Global Chief Digital Officer ", boss);
-        //}
-
+            string output = DomainFactory.Generator.GetSentences(1)[0];
+            Assert.AreEqual("The Global Chief Digital Officer 200% achieves efficiencies going forward.", output);
+        }
     }
 }
