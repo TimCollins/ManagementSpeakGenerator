d98273b91e1e3859e77c7880c38b8f33bd664d08
Tim Collins <tim@timcollins.ie>
2014-09-09 10:27:07 +0100

    Removed test code.

--- src/MSG.ConsoleApp/Program.cs
+++ src/MSG.ConsoleApp/Program.cs
@@ -8,26 +8,8 @@ namespace MSG.ConsoleApp
 {
     class Program
     {
-// If this is a debug build then define _testCounter
-#if DEBUG
-        private static int _testCounter;
-#endif
-
         static void Main(string[] args)
         {
-            // Call the method that should only exist in a debug build.
-            // In release mode this line will generate an error because method arguments
-            // must be well defined at the point of the method call. This holds true even
-            // if the method itself is going to be removed. In this case the compiler knows
-            // nothing about _testCounter because its declaration has been omitted so the 
-            // initial lexical analysis of the file fails even though Test() itself is going
-            // to be removed.
-            // See http://blogs.msdn.com/b/ericlippert/archive/2009/09/10/what-s-the-difference-between-conditional-compilation-and-the-conditional-attribute.aspx
-            // and http://stackoverflow.com/q/3788605/137001
-            // also http://stackoverflow.com/q/2104099
-
-            Test(_testCounter++);
-
             Console.WriteLine("Management-Speak Generator.");
 
             Console.WriteLine("Writing test data to file...");
@@ -46,13 +28,6 @@ namespace MSG.ConsoleApp
 
             Console.WriteLine("Data written to {0}", fileName);
             Util.WaitForEscape();
-        }
-
-        // Define a method that should only exist in a debug build.
-        [Conditional("DEBUG")]
-        private static void Test(int i)
-        {
-            // Do stuff
         }
     }
 }
